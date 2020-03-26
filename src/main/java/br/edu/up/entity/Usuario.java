a46ea44765e2177675e72f620ace253e2f4e123e
Lucas Fernandes <lucasfernandes_06@hotmail.com>
2020-03-26 17:32:33 -0300

    Exemplo criaçao catalogo

---
+++ src/main/java/br/edu/up/entity/Usuario.java
@@ -0,0 +1,46 @@
+package br.edu.up.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "usuario") 
+public class Usuario {
+ @Id
+ @GeneratedValue(strategy = GenerationType.IDENTITY)
+ @Column(name = "cod")
+	private Integer id;
+ 
+ @Column(name = "nome")
+	private String nome;
+ 
+ @Column(name = "senha")
+	private String senha;
+ 
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+	
+	
+	
+}
